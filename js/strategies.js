b1c25842158931d2cc22fee8526e88ee3807e60e
Antoine Toubhans <antoine@toubhans.org>
2017-01-03 19:56:20 +0100

    Add less dumb strategy

--- js/strategies.js
+++ js/strategies.js
@@ -1,23 +1,56 @@
+function TotalDumbStrategy() {
+}
+
+TotalDumbStrategy.prototype.predict = function (grid) {
+  return Math.floor((Math.random() * 4));
+}
+
+// ---------------------------------------------------------
+
 function DumbStrategy() {
 }
 
 DumbStrategy.prototype.predict = function (grid) {
-  return Math.floor((Math.random() * 4));
+  if (Math.random() > 0.4) return 1;
+  if (Math.random() > 0.4) return 2;
+  if (Math.random() > 0.4) return 0;
+
+  return 3;
 }
+
+// ---------------------------------------------------------
 
 function LessDumbStrategy() {
 }
 
-LessDumbStrategy.prototype.canPlayRight = function (grid) {
-  return Math.random() > 0.4;
+LessDumbStrategy.prototype.canPlayRow = function (c1, c2, c3, c4) {
+  return (c4 === null && c3 !== null)
+    || (c3 === null && c2 !== null)
+    || (c2 === null && c1 !== null)
+    || (c1 && c2 && c1.value === c2.value)
+    || (c2 && c3 && c2.value === c3.value)
+    || (c3 && c4 && c3.value === c4.value) ;
 }
 
-LessDumbStrategy.prototype.canPlayDown = function (grid) {
-  return Math.random() > 0.5;
+LessDumbStrategy.prototype.canPlayRight = function (grid) {
+  var self = this;
+  return [0, 1, 2, 3].some(function (i) {
+    return self.canPlayRow(grid.cells[0][i], grid.cells[1][i], grid.cells[2][i], grid.cells[3][i]);
+  });
 }
 
 LessDumbStrategy.prototype.canPlayUp = function (grid) {
-  return Math.random() > 0.6;
+  var self = this;
+  return [0, 1, 2, 3].some(function (i) {
+    return self.canPlayRow(grid.cells[i][3], grid.cells[i][2], grid.cells[i][1], grid.cells[i][0]);
+  });
+}
+
+LessDumbStrategy.prototype.canPlayDown = function (grid) {
+  var self = this;
+  return [0, 1, 2, 3].some(function (i) {
+    return self.canPlayRow(grid.cells[i][0], grid.cells[i][1], grid.cells[i][2], grid.cells[i][3]);
+  });
 }
 
 LessDumbStrategy.prototype.predict = function (grid) {
